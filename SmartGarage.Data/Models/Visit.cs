4ca669b8ae05975e883b2a88018f1e6f613c8086
Plamen Nedelchev <plamen.nedelchev.a54@learn.telerikacademy.com>
2024-01-30 20:03:08 +0200

    add visit model

---
+++ SmartGarage.Data/Models/Visit.cs
@@ -0,0 +1,27 @@
+﻿using SmartGarage.WebAPI.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SmartGarage.Data.Models
+{
+	public class Visit
+	{
+		[Key]
+		public int Id { get; set; }
+
+		[Required]
+		public DateTime Date { get; set; } = DateTime.UtcNow;
+
+		[Required]
+		public string UserId { get; set; } = null!;
+
+		[ForeignKey(nameof(UserId))]
+		public AppUser User { get; set; } = null!;
+
+		[Required]
+		public int VehicleId { get; set; }
+
+		[ForeignKey(nameof(VehicleId))]
+        public Vehicle Vehicle { get; set; } = null!;
+	}
+}
