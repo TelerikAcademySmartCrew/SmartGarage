b7e30438b87bcbd45b6edae54b843b965e42297d
Plamen Nedelchev <plamen.nedelchev.a54@learn.telerikacademy.com>
2024-01-30 18:21:38 +0200

    implement methods in service repository

--- SmartGarage.Data/Repositories/Contracts/ICarServiceRepository.cs
+++ SmartGarage.Data/Repositories/Contracts/ICarServiceRepository.cs
@@ -1,22 +1,22 @@
 ﻿using SmartGarage.Data.Models;
-using SmartGarage.Data.Models.DTOs;
 using SmartGarage.WebAPI.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SmartGarage.Data.Repositories.Contracts
 {
-    public interface IServiceRepository
+	public interface ICarServiceRepository
     {
-        Task<IList<Service>> GetAllAsync(ServiceQueryParameters serviceQueryParameters);
+        Task<IList<Service>> GetAllAsync();
+
         Task<Service> GetServiceByIdAsync(int serviceId);
+
         Task<IList<Service>> GetServicesByUserIdAsync(string userId);
+
         Task<IList<Service>> GetServicesByVehicleIdAsync(int vehicleId);
+
         Task<Service> CreateServiceAsync(Service service, AppUser currentUser);
+
         Task<Service> UpdateServiceAsync(int serviceId, Service service);
+
         Task DeleteServiceAsync(int serviceId);
     }
 }
