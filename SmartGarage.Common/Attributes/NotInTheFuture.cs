f0b836185dd572d8e0c722dd96a65e4fb1bcd2d8
Plamen Nedelchev <plamen.nedelchev.a54@learn.telerikacademy.com>
2024-01-31 11:29:07 +0200

    implement custom validation attribute that checks the input year of the vehicle

---
+++ SmartGarage.Common/Attributes/NotInTheFuture.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartGarage.Common.Attributes
+{
+	[AttributeUsage(AttributeTargets.Property, AllowMultiple = false)]
+	public class NotInTheFuture : ValidationAttribute
+    {
+		public override bool IsValid(object? value)
+		{
+			if (value is int year)
+			{
+				return year <= DateTime.UtcNow.Year;
+			}
+
+			return false;
+		}
+	}
+}
