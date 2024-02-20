058b33fa0d8d7f62a8d2190843ed64121592923f
Plamen <plamen.nedelchev.a54@learn.telerikacademy.com>
2024-02-21 00:07:52 +0200

    implement vehicle brand api controller

---
+++ SmartGarage.Common/Models/RequestDtos/VehicleBrandRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SmartGarage.Common.Models.RequestDtos
+{
+    public class VehicleBrandRequestDto
+    {
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
--- SmartGarage.WebAPI/Controllers/VehicleBrandApiController.cs
+++ SmartGarage.WebAPI/Controllers/VehicleBrandApiController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SmartGarage.Common.Exceptions;
+using SmartGarage.Common.Models.RequestDtos;
 using SmartGarage.Services.Contracts;
+using SmartGarage.Utilities.Mappers.Contracts;
 
 namespace SmartGarage.WebAPI.Controllers
 {
@@ -8,37 +10,58 @@ namespace SmartGarage.WebAPI.Controllers
     [Route("api/brands")]
     public class VehicleBrandApiController : ControllerBase
     {
-        private readonly IBrandService brandService;
-        private readonly IModelService modelService;
+        private readonly IBrandService vehicleBrandService;
+        private readonly IVehicleBrandMapper vehicleBrandMapper;
 
         public VehicleBrandApiController(
-            IBrandService brandService,
-            IModelService modelService)
+            IBrandService vehicleBrandService,
+            IVehicleBrandMapper vehicleBrandMapper)
         {
-            this.brandService = brandService;
-            this.modelService = modelService;
+            this.vehicleBrandService = vehicleBrandService;
+            this.vehicleBrandMapper = vehicleBrandMapper;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] VehicleBrandRequestDto dto)
+        {
+            var brand = this.vehicleBrandMapper.Materialize(dto);
+            var created = await this.vehicleBrandService.CreateAsync(brand);
+
+            return Ok(created);
         }
 
         [HttpGet]
-        public async Task<IActionResult> GetBrandsAsync()
+        public async Task<IActionResult> GetAll()
         {
-            var brands = await this.brandService.GetAllAsync();
-
+            var brands = await this.vehicleBrandService.GetAllAsync();
             return Ok(brands);
-        }
+        }        
 
-        //api/brands/models?brandName=bmw
-        [HttpGet("models")]
-        public async Task<IActionResult> GetAllBrandModels([FromQuery] string brandName)
+        [HttpGet]
+        public async Task<IActionResult> GetByName([FromQuery] string name)
         {
             try
             {
-                var brand = await this.brandService.GetByNameAsync(brandName);
-                return Ok(brand.Models);
+                var brand = await this.vehicleBrandService.GetByNameAsync(name);
+                return Ok(brand);
             }
-            catch (EntityNotFoundException e)
+            catch (EntityNotFoundException ex)
             {
-                return NotFound(e.Message);
+                return NotFound(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetById([FromQuery] Guid id)
+        {
+            try
+            {
+                var brand = await this.vehicleBrandService.GetByIdAsync(id);
+                return Ok(brand);
+            }
+            catch (EntityNotFoundException ex)
+            {
+                return NotFound(ex.Message);
             }
         }
     }
